56fc26eaabfc62557acac808aebfbe0fff5ccb9e
Martin Wong <Martin Wong>
2016-11-07 11:53:35 -0500

    Adding build folders as well as changing the motion controller's parent.

---
+++ Source/UnrealTech/UnrealTech.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealTech.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealTech, "UnrealTech" );
