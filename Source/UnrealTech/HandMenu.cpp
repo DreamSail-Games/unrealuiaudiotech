311eddeb99ff1ed32af1b726c8dc7587f4466d55
Martin Wong <Martin Wong>
2016-11-09 15:50:52 -0500

    changing which spawn overload to use

--- Source/UnrealTech/HandMenu.cpp
+++ Source/UnrealTech/HandMenu.cpp
@@ -9,6 +9,7 @@ void UHandMenu::CreatePlayer(AActor* player)
 
 	UE_LOG(LogTemp, Warning, TEXT("Trying to create a player"));
 	FActorSpawnParameters SpawnParams;
-	FTransform location = player->GetTransform();
-	AMusicPlayerContainer* myRef = GetWorld()->SpawnActor<AMusicPlayerContainer>(MusicPlayerBP, location, SpawnParams);
+	FVector location = player->GetActorLocation();
+	FRotator rotation = player->GetActorRotation();
+	AMusicPlayerContainer* myRef = GetWorld()->SpawnActor<AMusicPlayerContainer>(MusicPlayerBP, location,rotation, SpawnParams);
 }
